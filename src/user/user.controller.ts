cf28d2b864e8721555924eed0285fd515c63770a
BennyDRG <bennyboy260@gmail.com>
2023-11-02 21:44:43 +0100

    Copy-Twitter

---
+++ src/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto';
+import { UserService } from './user.service';
+import { CommentService } from 'src/comment/comment.service';
+
+@Controller('user')
+export class UserController {
+  constructor(
+    private userService: UserService,
+    private commentService: CommentService,
+  ) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get(':id/comment')
+  findUserComment(@Param('id') id: string) {
+    return this.commentService.findUserComment(id);
+  }
+}
